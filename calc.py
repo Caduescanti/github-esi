62864e6dabf4d396aa2ddbb6d24559d72ea74e8e
Carlos Eduardo <caduoskanti@gmail.com>
2025-05-15 11:14:48 -0300

    corrige erro  Your branch is up to date with 'origin/hotfix/div0'.

--- calc.py
+++ calc.py
@@ -1,6 +1,8 @@
 def sub(x, y):
       return x - y
 def sum(x, y):
+      """Soma dois numeros e retorna o resultado 
+      """
       return x + y
 def div(x, y):
     return x / y if y != 0 else None
